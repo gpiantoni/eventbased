06ea73646087594cc0e248efeb25623035a08498
Gio <github@gpiantoni.com>
2012-03-06 08:41:15 +0100

    deals with no layout, sensor pos. Better help for pow

--- pow_grand.m
+++ pow_grand.m
@@ -1,5 +1,32 @@
 function pow_grand(cfg)
 %POW_GRAND grand power average
+% 1) read single subject-data and create gpow in cfg.dpow
+% 2) do statistics for condition indicated by cfg.poweffect, to create powpeak
+% 3) plot the topoplot over time and singleplot for some electrodes
+%
+% CFG
+%  .cond: name to be used in projects/PROJNAME/subjects/0001/MOD/CONDNAME/
+%  .test: a cell with the condition defined by redef.
+%  .sens.layout: file with layout. It should be a mat containing 'layout'
+%                If empty, it does not plot topo.
+%
+%  .dpow: directory to save POW data
+%  .poweffect: effect of interest to create powpeak. If empty, no stats.
+%
+%  .gpow.chan(1).name = 'name of channel group';
+%  .gpow.chan(1).chan =  cell with labels of channels of interest
+%
+% OUT
+%  [cfg.dpow 'COND_grandpow']: timelock analysis for all subjects
+%  [cfg.dpow 'COND_powpeak']: significant peaks in the POW
+%
+% FIGURES
+%  gpow_tfr_c01_COND: time-frequency plot POW, for each condition, for one channel group
+%  gpow_val_c01_FREQ: singleplot POW, all conditions, for one channel group, one frequency
+%  gpow_topo_COND_FREQ: topoplot POW for each condition and frequency, over time
+%
+% Part of EVENTBASED group-analysis
+% see also ERP_SUBJ, ERP_GRAND, ERPSOURCE_SUBJ, ERPSOURCE_GRAND
 
 %---------------------------%
 %-start log
@@ -56,7 +83,9 @@ save([cfg.dpow cfg.proj '_grandpow'], 'gpow')
 
 %-----------------------------------------------%
 %-stats and plots
-load(cfg.sens.layout, 'layout');
+if ~isempty(cfg.sens.layout)
+  load(cfg.sens.layout, 'layout');
+end
 
 if ~isempty(gpow)
   
@@ -151,59 +180,61 @@ if ~isempty(gpow)
       [~, logfile] = fileparts(cfg.log);
       system(['ln ' cfg.log filesep pngname '.png ' cfg.rslt pngname '_' logfile '.png']);
       %--------%
-
+      
     end
     %-----------------%
     
   end
   %---------------------------%
-    
+  
   %---------------------------%
   %-topoplotTFR (loop over tests)
-  for t = 1:numel(cfg.test)
-    
-    %-----------------%
-    %-loop over freq
-    for f = 1:numel(cfg.gpow.freq)
+  if ~isempty(cfg.sens.layout)
+    for t = 1:numel(cfg.test)
       
-      %--------%
-      %-figure
-      h = figure;
-      set(h, 'Renderer', 'painters')
-      %--------%
+      %-----------------%
+      %-loop over freq
+      for f = 1:numel(cfg.gpow.freq)
+        
+        %--------%
+        %-figure
+        h = figure;
+        set(h, 'Renderer', 'painters')
+        %--------%
+        
+        %--------%
+        %-plot
+        cfg5 = [];
+        cfg5.parameter = 'powspctrm';
+        cfg5.layout = layout;
+        cfg5.xlim = gpow{t}.time;
+        cfg5.ylim = cfg.gpow.freq{f};
+        cfg5.zlim = 'maxabs';
+        cfg5.style = 'straight';
+        cfg5.marker = 'off';
+        cfg5.comment = 'xlim';
+        cfg5.commentpos = 'title';
+        ft_topoplotER(cfg5, gpow{t});
+        %--------%
+        
+        %--------%
+        %-save and link
+        condname = regexprep(cfg.test{t}, '*', '');
+        freqname = sprintf('f%02.f-%02.f', cfg.gpow.freq{f}(1), cfg.gpow.freq{f}(2));
+        
+        pngname = sprintf('gpow_topo_%s_%s', condname, freqname);
+        saveas(gcf, [cfg.log filesep pngname '.png'])
+        close(gcf); drawnow
+        
+        [~, logfile] = fileparts(cfg.log);
+        system(['ln ' cfg.log filesep pngname '.png ' cfg.rslt pngname '_' logfile '.png']);
+        %--------%
+        
+        %--------%
+      end
+      %-----------------%
       
-      %--------%
-      %-plot
-      cfg5 = [];
-      cfg5.parameter = 'powspctrm';
-      cfg5.layout = layout;
-      cfg5.xlim = gpow{t}.time;
-      cfg5.ylim = cfg.gpow.freq{f};
-      cfg5.zlim = 'maxabs';
-      cfg5.style = 'straight';
-      cfg5.marker = 'off';
-      cfg5.comment = 'xlim';
-      cfg5.commentpos = 'title';
-      ft_topoplotER(cfg5, gpow{t});
-      %--------%
-      
-      %--------%
-      %-save and link
-      condname = regexprep(cfg.test{t}, '*', '');
-      freqname = sprintf('f%02.f-%02.f', cfg.gpow.freq{f}(1), cfg.gpow.freq{f}(2));
-
-      pngname = sprintf('gpow_topo_%s_%s', condname, freqname);
-      saveas(gcf, [cfg.log filesep pngname '.png'])
-      close(gcf); drawnow
-      
-      [~, logfile] = fileparts(cfg.log);
-      system(['ln ' cfg.log filesep pngname '.png ' cfg.rslt pngname '_' logfile '.png']);
-      %--------%
-
-      %--------%
     end
-    %-----------------%
-    
   end
   %---------------------------%
   
