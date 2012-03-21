db9e0b7a4672cf0b1eeac6d90c98812b7a315d10
Gio <github@gpiantoni.com>
2012-03-21 09:46:42 +0100

    detect outliers using standard error at each TF point

--- pow_subj.m
+++ pow_subj.m
@@ -11,15 +11,20 @@ function pow_subj(cfg, subj)
 %  .dpow: directory to save POW data
 %
 %  .pow: a structure with cfg to pass to ft_freqanalysis
-%  .pow.bl.baseline: two scalars with baseline windows (if empty, no baseline)
+%  .pow.outliers: logical (print tables with number of points above a
+%  certain number of standard deviation, experimental code)
+%  .pow.outliersthr: if outliers is true, this is the threshold in sd to reject a trial (if empty, no rejection)
+%
+%  .pow.bl: if empty, no baseline. Otherwise:
+%  .pow.bl.baseline: two scalars with baseline windows
 %  .pow.bl.baselinetype: type of baseline ('relchange')
 %
 % OUT
 %  [cfg.dpow 'pow_001_TEST']: power analysis for single-subject
 %
 % Part of EVENTBASED single-subject
-% see also ERP_SUBJ, ERP_GRAND, ERPSOURCE_SUBJ, ERPSOURCE_GRAND, 
-% POW_SUBJ, POW_GRAND, POWSOURCE_SUBJ, POWSOURCE_GRAND, 
+% see also ERP_SUBJ, ERP_GRAND, ERPSOURCE_SUBJ, ERPSOURCE_GRAND,
+% POW_SUBJ, POW_GRAND, POWSOURCE_SUBJ, POWSOURCE_GRAND,
 % POWCORR_SUBJ, POWCORR_GRAND,
 % CONN_SUBJ, CONN_GRAND, CONN_STAT
 
@@ -67,29 +72,104 @@ for k = 1:numel(cfg.test)
     
   end
   %-----------------%
-
+  
   %-----------------%
   %-calculate power
   cfg2 = cfg.pow;
   cfg2.feedback = 'none';
-  data = ft_freqanalysis(cfg2, data);
+  
+  if cfg.pow.outliers
+    cfg2.keeptrials = 'yes';
+  end
+  
+  freq = ft_freqanalysis(cfg2, data);
   
   if isfield(cfg.pow, 'toi')
-    data.time = cfg.pow.toi;
+    freq.time = cfg.pow.toi;
   end
   %-----------------%
   
   %-----------------%
-  %-fix baseline
-  if ~isempty(cfg.pow.bl.baseline)
-    cfg3 = cfg.pow.bl;
-    freq = ft_freqbaseline(cfg3, data);
+  %-deal with outliers
+  if cfg.pow.outliers
     
-  else
-    freq = data;
+    %-------%
+    %-freqtrl: freq with single trials, freq: averaged freq
+    freqtrl = freq;
+    
+    cfg4 = [];
+    cfg4.jackknife = 'yes'; % more robust against outliers
+    freq = ft_freqdescriptives(cfg4, freqtrl);
+    freq.powspctrmsem = freq.powspctrmsem * sqrt(size(freqtrl.powspctrm,1)); % sem = sd / sqrt(n_trials)
+    %-------%
+    
+    %-------%
+    %-compute distance in sd for each TFR point
+    powspctrm = bsxfun(@minus, freqtrl.powspctrm, shiftdim(freq.powspctrm, -1));
+    powspctrm = bsxfun(@rdivide, powspctrm, shiftdim(freq.powspctrmsem, -1));
+    
+    trlsd = zeros(size(powspctrm,1), 1);
+    for i = 1:size(powspctrm,1)
+      i_powspctrm = powspctrm(i, :, :, :);
+      trlsd(i) = nanstd(i_powspctrm(:),1);
+    end
+    
+    freq = rmfield(freq, 'powspctrmsem'); % you don't need this extra field
+    %-------%
+    
+    %-------%
+    %-reject outliers
+    if isfield(cfg.pow, 'outliersthr') && ~isempty(cfg.pow.outliersthr)
+      goodtrl = find(trlsd < cfg.pow.outliersthr);
+      badtrl = find(trlsd >= cfg.pow.outliersthr);
+      
+      output = [output sprintf('Cond %s Rejecting %5.f outliers out of %5.f trials, remaining %5.f trials\n', ...
+        condname, numel(badtrl), numel(trlsd), numel(goodtrl))];
+      
+      cfg4 = [];
+      cfg4.trials = goodtrl;
+      freq = ft_freqdescriptives(cfg4, freqtrl);
+      
+    end
+    %-------%
+    
+    %-------%
+    %-plot feedback on montage
+    figure
+    plot(trlsd, '.')
+    
+    if isfield(cfg.pow, 'outliersthr') && ~isempty(cfg.pow.outliersthr)
+      hold on
+      plot(badtrl, trlsd(badtrl), '+r')
+    end
+    
+    legend('trials', 'bad trials')
+    title('outliers')
+    xlabel('n trials')
+    ylabel('s.d.')
+    
+    pngfile = [cfg.log filesep 'powoutliers_' sprintf('%03.f', subj) '_' condname '.png'];
+    saveas(gcf, pngfile);
+    close(gcf); drawnow
+    %-------%
     
   end
   %-----------------%
+  
+  %-----------------%
+  %-baseline correction
+  if ~isempty(cfg.pow.bl) && cfg.pow.bl.singletrial
+    cfg3 = [];
+    cfg3.baseline = cfg.pow.bl.baseline;
+    cfg3.baselinetype = cfg.pow.bl.baselinetype;
+    freqtrl = ft_freqbaseline(cfg3, freq);
+    
+  else
+    freqtrl = freq;
+    
+  end
+  %-----------------%
+  
   save([cfg.dpow outputfile], 'freq')
   
 end
