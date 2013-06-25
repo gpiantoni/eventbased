60942296a4d8ea7811d1c62757caa84927d1f598
Gio <github@gpiantoni.com>
2013-06-25 20:11:36 +0200

    ENH: erpsource_grand works with info, opt

--- erpsource_grand.m
+++ erpsource_grand.m
@@ -1,41 +1,16 @@
 function erpsource_grand(info, opt)
-%ERPSOURCE_GRAND group-analysis of ERP source data
+%ERPSOURCE_GRAND group-level analysis of ERP source data
 %
-% CFG
-%-Average
-%  .nick: NICK to save files specific to each NICK
-%  .log: name of the file and directory with analysis log
-%  .subjall: index of the number of subjects
-%
+% INFO
+%  .log: name of the file and directory to save log
 %  .derp: directory with ERP data
-%  .erpsource.cond: cell to make averages
+%  .mri.template: template of the averaged MRI
 %
-%  .sourcespace: 'surface' 'volume' 'volume_warp'
-%  (if cfg.sourcespace == 'surface')
-%  .SUBJECTS_DIR: where the Freesurfer data is stored (like the environmental variable)
-%  .surfdownsample: ratio of downsampling of the surface
-%
-%-Statistics
-%  The comparison is always against baseline. If you want to compare
-%  conditions, use erpstats_subj.
-%
-%  .erpsource.areas: how to speficy peaks to analyze, 'manual' or 'erp_peak' (peaks from granderp)
-%    if 'manual'
-%      .erpsource.erp_peak(1).name: string ('name_of_the_time_window')
-%      .erpsource.erp_peak(1).time: scalar (center of the time window in s)
-%      .erpsource.erp_peak(1).wndw: scalar (length of the time window in s)
-%    if 'erp_peak'
-%      .erp.refcond: string of the comparison whose peaks will be localized
-%
-% Options for reportsource:
-%  .erpsource.clusterstatistics: 'maxsize' or 'max'
-%  .erpsource.clusteralpha: level to select sensors (default 0.05)
-%                           it can be a string in format '5%' to take top 5 voxels and put them in a cluster.
-%  .erpsource.maxvox: max number of significant voxels to be used in soupeak
-%  .erpsource.clusterthr: threshold to report clusters in output
-%  .erpsource.atlas: index of the atlas to report labels
-%
-%  .rslt: directory images are saved into
+% CFG.OPT.
+%  .comp*: comparisons to test (cell within cell, e.g. {{'cond1'} {'cond2'}})
+%          You can only have one cell (while in ERP_GRAND you can have 2),
+%          because statistics with different filters is not correct. Use ERPSTAT_SUBJ to reuse filters
+%  .peak*: peak of interest (see GET_PEAK)
 %
 % IN
 %  [info.derp 'erpsource_SUBJ_COND'] 'erpsource_subj_A': source data for period of interest for each subject
@@ -47,8 +22,10 @@ function erpsource_grand(info, opt)
 % FIGURES
 %  gerp_peak_COND_ERPPEAK: 3d plot of the source for one peak
 %
+% * indicates obligatory parameter
+%
 % Part of EVENTBASED group-analysis
-% see also ERP_SUBJ, ERP_GRAND, 
+% see also ERP_SUBJ, ERP_GRAND,
 % ERPSOURCE_SUBJ, ERPSOURCE_GRAND, ERPSTAT_SUBJ, ERPSTAT_GRAND,
 % POW_SUBJ, POW_GRAND, POW_GRP, POWCORR_SUBJ, POWCORR_GRAND,
 % POWSOURCE_SUBJ, POWSOURCE_GRAND, POWSTAT_SUBJ, POWSTAT_GRAND,
@@ -61,54 +38,54 @@ output = sprintf('%s began at %s on %s\n', ...
 tic_t = tic;
 %---------------------------%
 
-erp_peak = get_peak(cfg, 'erp');
+erp_peak = get_peak(info, opt.peak, 'erp');
 
 %---------------------------%
-%-prepare two hemisphere if surface
-if strcmp(cfg.sourcespace, 'surface')
-  hemi = {'lh' 'rh'};
-  if ~isfield(cfg, 'surfdownsample'); cfg.surfdownsample = 0.01; end
-  
-  %-----------------%
-  %-mesh for stats and plotting
-  sdir = sprintf('%s%s/%s', cfg.SUBJECTS_DIR, 'fsaverage', 'surf/');
-  [avgsphere, template] = read_avgsurf(sdir, cfg.surfdownsample);
-  %-----------------%
-  
-else
-  hemi = {''}; % no hemisphere
-  
-  %-----------------%
-  %-load template
-  template = ft_read_mri(cfg.template.volume);
-  %-----------------%
-  
-end
+%-load template TODO: find elegant solution for this
+load(info.mri.template)
+template = mri;
+% template = ft_read_mri(info.mri.template);
 %---------------------------%
 
 %---------------------------------------------------------%
 %-statistics for main effects
 %-----------------------------------------------%
 %-loop over conditions
-for k = 1:numel(cfg.erpsource.cond)
-  cond     = cfg.erpsource.cond{k};
-  condname = regexprep(cond, '*', '');
+for t = 1:numel(opt.comp)
   
-  %-----------------%
-  %-file for each cond
-  [outtmp data] = load_subj(info, 'erpsource', cond);
-  output = [output outtmp];
-  if isempty(data); continue; end
-  %-----------------%
-  
-  %-----------------%
-  %-pow or coh
-  if isfield(data{1}.avg, 'coh') % coh wins
-    cfg.erpsource.parameter = 'coh';
+  %---------------------------%
+  %-statistics for effects of interest
+  if numel(opt.comp{t}) == 1
+    
+    %-----------------%
+    %-compare against zero
+    cond = opt.comp{t}{1};
+    comp = regexprep(cond, '*', '');
+    output = sprintf('%s\n   COMPARISON %s\n', output, cond);
+    %-----------------%
+    
+    %-----------------%
+    %-file for each cond
+    [outtmp data] = load_subj(info, 'erpsource', cond);
+    output = [output outtmp];
+    if isempty(data); continue; end
+    %-----------------%
+    
+    %-----------------%
+    %-erp or coh
+    if isfield(data{1}.avg, 'coh') % coh wins
+      opt.parameter = 'coh';
+    else
+      opt.parameter = 'pow';
+    end
+    %-----------------%
+    
   else
-    cfg.erpsource.parameter = 'pow';
+    output = [output sprintf('It does not make sense to compare conditions using different filters\nPlease use erpstat for comparisons\n')];
+    continue
+    
   end
-  %-----------------%
+  %---------------------------%
   
   %-------------------------------------%
   %-loop over peaks
@@ -117,56 +94,29 @@ for k = 1:numel(cfg.erpsource.cond)
     output = sprintf('%s\n%s:\n', output, erp_peak(p).name);
     
     %---------------------------%
-    %-loop over hemisphere
-    for h = 1:numel(hemi)
-      
-      %-----------------%
-      %-interpolate to average sphere
-      if strcmp(cfg.sourcespace, 'surface')
-        tmpcfg = [];
-        %tmpcfg.method = TODO: it's possible to specify other interpolation methods
-        tmpcfg.parameter = ['avg.' cfg.erpsource.parameter];
-        
-        for i1 = 1:size(data,1)
-          for i2 = 1:size(data,2)
-            data{i1,i2, p, h} = ft_sourceinterpolate(tmpcfg, data{i1,i2,p,h}, avgsphere{h});
-          end
-        end
-      end
-      %-----------------%
-      
-      %-----------------%
-      %-grand average
-      tmpcfg = [];
-      tmpcfg.keepindividual = 'yes';
-      tmpcfg.parameter = cfg.erpsource.parameter;
-      gerpsouPre = ft_sourcegrandaverage(tmpcfg, data{:,1,p,h});
-      gerpsource = ft_sourcegrandaverage(tmpcfg, data{:,2,p,h});
-      %-----------------%
-      
-      %-----------------%
-      %-do stats
-      cfg.erpsource.channeighbstructmat = avgsphere{h}.neigh;
-      [erpsource{p,h} outtmp] = report_source(cfg.erpsource, gerpsource, gerpsouPre);
-      erpsource{p,h}.name = erp_peak(p).name;
-      output = [output outtmp];
-      %-----------------%
-      
-    end
+    %-----------------%
+    %-grand average
+    cfg = [];
+    cfg.keepindividual = 'yes';
+    cfg.parameter = opt.parameter;
+    gerpsouPre = ft_sourcegrandaverage(cfg, data{:,1,p});
+    gerpsource = ft_sourcegrandaverage(cfg, data{:,2,p});
+    %-----------------%
+    
+    %-----------------%
+    %-do stats
+    [erpsource{p,1} outtmp] = report_source(opt, gerpsource, gerpsouPre);
+    erpsource{p}.name = erp_peak(p).name;
+    output = [output outtmp];
+    %-----------------%
     
     %-----------------%
     %-plot source
-    h = figure;
-    if strcmp(cfg.sourcespace, 'surface')
-      plot_surface(erpsource(p,:), template, 'stat')
-      
-    else
-      plot_volume(erpsource(p, :), template, 'stat')
-      
-    end
+    h = figure('vis', 'off');
+    plot_volume(erpsource(p, :), template, 'stat')
     
     %--------%
-    pngname = sprintf('gerp_peak_%s_%s', condname, erp_peak(p).name);
+    pngname = sprintf('gerp_peak_%s_%s', comp, erp_peak(p).name);
     saveas(h, [info.log filesep pngname '.png'])
     close(h); drawnow
     
@@ -179,13 +129,13 @@ for k = 1:numel(cfg.erpsource.cond)
   end
   %-------------------------------------%
   
-  %-----------------%
+  %---------------------------%
   %-save
   for p = 1:numel(erpsource)
     erpsource{p}.cfg = []; % this is huge
   end
-  save([info.derp 'erpsource_' condname], 'erpsource', '-v7.3')
-  %-----------------%
+  save([info.derp 'erpsource_' comp], 'erpsource', '-v7.3')
+  %---------------------------%
   
 end
 %---------------------------%
